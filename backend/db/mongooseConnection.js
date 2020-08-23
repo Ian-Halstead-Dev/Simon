11e79f757b84bf2f78d0d7cfa5430f0a30a3ab70
Ian Halstead <iwhalstead10@gmail.com>
2020-08-22 21:50:36 -0700

    capitilize envs

--- backend/db/mongooseConnection.js
+++ backend/db/mongooseConnection.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionUrl = process.env.dbName;
+const connectionUrl = process.env.DBNAME;
 
 const connectToDb = () => {
 	mongoose.connect(connectionUrl, {
