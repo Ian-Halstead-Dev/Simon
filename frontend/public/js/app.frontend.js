b50665a4d3a72d8fab89cdfa59d1690192f9565b
Ian Halstead <iwhalstead10@gmail.com>
2020-08-25 20:15:49 -0700

    made the game faster

--- frontend/public/js/app.frontend.js
+++ frontend/public/js/app.frontend.js
@@ -347,7 +347,7 @@ var GameScreen = function (_React$Component4) {
 				var color = sequence[i];
 				_this7.pressButton(color);
 				i++;
-			}, 1000);
+			}, 500);
 		}
 	}, {
 		key: 'pressButton',
